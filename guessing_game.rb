7ce9b7f68acaa345386a6cb8b28fab868c10516f
Danai Adkisson <dadkisso@live.com>
2015-05-05 16:13:08 -0400

    file now runs and quits after 5 attempts or if user guesses correct

--- guessing_game.rb
+++ guessing_game.rb
@@ -2,24 +2,26 @@ def random
  rand(1..100)
 end
 var=random
-def multi_guess
-  if guess == given_nums
-    puts "you alread guessed that number!"
-  end
-end
+#def multi_guess
+#  if guess == given_nums
+  #  puts "you alread guessed that number!"
+#  end
+#multi_guess=multi
 
 puts var
 
 #(0..4).each do |counter|
-puts "Guess a number between 1-100."
-guess = gets.chomp.to_i
+
 
 #until guess == var
 given_nums=[]
- (1..5).each do |i|
+(1..5).each do |i|
+
+
+  puts "Guess a number between 1-100!"
+  guess = gets.chomp.to_i
+
   given_nums << guess
-
-
 
   if guess > var
     puts "you guessed high, try again."
@@ -27,14 +29,11 @@ given_nums=[]
   elsif guess < var
     puts "you guessed low, try again."
     puts "so far you have guessed #{given_nums}"
-  elsif guess == var
+  else guess == var
     puts "#{var} is correct, Congratulations!!!"
+    break
   end
 
-    puts "Guess a number between 1-100!"
-    guess = gets.chomp.to_i
+
 
 end
-
-
-puts "#{var} is correct, Congratulations!!!"
