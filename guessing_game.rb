7e9b329319be042da98f19b4031065ba842de2ec
Danai Adkisson <dadkisso@live.com>
2015-05-05 14:27:16 -0400

    progam will now loop and close out once you have picked the correct number

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,17 +1,27 @@
 def random
-  r = rand(1..100)
-return r
+ rand(1..100)
 end
 var=random
 
 puts var
 
 puts "Guess a number between 1-100."
-guess = gets.chomp
-guess_2 = guess.to_i
+guess = gets.chomp.to_i
+#guess_2 = guess.to_i
+until guess == var
+  while guess > var || guess < var
 
-if guess_2 > random
-  puts "you guessed high, try again."
-else
-  puts "you guessed low, try again."
+    if guess > var
+    puts "you guessed high, try again."
+    else
+    puts "you guessed low, try again."
+    end
+
+    puts "Guess a number between 1-100."
+    guess = gets.chomp.to_i
+  #guess_2 = guess.to_i
+
+
+  end
 end
+puts "#{var} is correct, Congratulations!!!"
