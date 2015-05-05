adff60f1692c2dc00ed2968a88fc127ed5efc70f
Danai Adkisson <dadkisso@live.com>
2015-05-05 16:49:26 -0400

    fully working

--- guessing_game.rb
+++ guessing_game.rb
@@ -2,18 +2,9 @@ def random
  rand(1..100)
 end
 var=random
-#def multi_guess
-#  if guess == given_nums
-  #  puts "you alread guessed that number!"
-#  end
-#multi_guess=multi
 
 puts var
 
-#(0..4).each do |counter|
-
-
-#until guess == var
 given_nums=[]
 (1..5).each do |i|
 
@@ -21,9 +12,11 @@ given_nums=[]
   puts "Guess a number between 1-100!"
   guess = gets.chomp.to_i
 
-  given_nums << guess
 
-  if guess > var
+  if given_nums.include?(guess)
+    puts "you've already guessed that number"
+    puts "so far you have guessed #{given_nums}"
+  elsif guess > var
     puts "you guessed high, try again."
     puts "so far you have guessed #{given_nums}"
   elsif guess < var
@@ -33,7 +26,8 @@ given_nums=[]
     puts "#{var} is correct, Congratulations!!!"
     break
   end
-
+  given_nums << guess
 
 
 end
+puts "the random number was #{var}"
